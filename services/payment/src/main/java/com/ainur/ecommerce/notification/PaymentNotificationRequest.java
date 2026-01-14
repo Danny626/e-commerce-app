52c0bebee8345e1b748a30820bba47f97a22da52
danny <live626@gmail.com>
2026-01-14 17:19:45 -0400

    servicio payment inicializado

---
+++ services/payment/src/main/java/com/ainur/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.ainur.ecommerce.notification;
+
+import com.ainur.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}

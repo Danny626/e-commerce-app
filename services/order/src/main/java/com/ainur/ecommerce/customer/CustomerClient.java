0c4130967c889e2e33ead65d090d440f7698a467
danny <live626@gmail.com>
2026-01-13 23:52:40 -0400

    agregado el servicio order, se agregó el contenedor kafka(la versión KRaft)

--- services/order/src/main/java/com/ainur/ecommerce/customer/CustomerClient.java
+++ services/order/src/main/java/com/ainur/ecommerce/customer/CustomerClient.java
@@ -1,11 +1,12 @@
 package com.ainur.ecommerce.customer;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
-@FeigndClient(
+@FeignClient(
         name = "customer-service",
         url = "${application.config.customer-url}"
 )
--- services/order/src/main/java/com/ainur/ecommerce/order/OrderService.java
+++ services/order/src/main/java/com/ainur/ecommerce/order/OrderService.java
@@ -2,7 +2,10 @@ package com.ainur.ecommerce.order;
 
 import com.ainur.ecommerce.customer.CustomerClient;
 import com.ainur.ecommerce.exception.BusinessException;
+import com.ainur.ecommerce.orderline.OrderLineRequest;
+import com.ainur.ecommerce.orderline.OrderLineService;
 import com.ainur.ecommerce.product.ProductClient;
+import com.ainur.ecommerce.product.PurchaseRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,22 +14,36 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderService {
 
+    private final OrderRepository repository;
     private final CustomerClient customerClient;
     private final ProductClient productClient;
+    private final OrderMapper mapper;
+    private final OrderLineService orderLineService;
 
-    public Integer createOrder(@Valid OrderRequest request) {
+    public Integer createOrder(OrderRequest request) {
         // check the customer --> OpenFeign
         var customer = this.customerClient.findCustomerById(request.customerId())
                 .orElseThrow(() -> new BusinessException("Cannot create order:: No customer exists with the provided ID"));
 
         // purchase the products --> product-ms (RestTemplate)
-
+        this.productClient.purchaseProducts(request.products());
 
         // persist order
+        var order = this.repository.save(mapper.toOrder(request));
 
         // persist order lines
+        for (PurchaseRequest purchaseRequest : request.products()) {
+            orderLineService.saveOrderLine(
+                    new OrderLineRequest(
+                            null,
+                            order.getId(),
+                            purchaseRequest.productId(),
+                            purchaseRequest.quantity()
+                    )
+            );
+        }
 
-        // start payment process
+        // todo start payment process
 
         // send the order confirmation --> notification-ms (kafka)
         return null;

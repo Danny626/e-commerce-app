44665b58e067762e9f89f4e555a7d0338cf0a430
danny <live626@gmail.com>
2026-01-16 16:59:53 -0400

    servicio gateway agregado, arreglos varios

--- services/notification/src/main/java/com/ainur/ecommerce/notification/NotificationRepository.java
+++ services/notification/src/main/java/com/ainur/ecommerce/notification/NotificationRepository.java
@@ -1,6 +1,5 @@
 package com.ainur.ecommerce.notification;
 
-import com.ainur.ecommerce.kafka.payment.PaymentConfirmation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface NotificationRepository extends MongoRepository<Notification, String> {
